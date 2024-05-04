c8b6327af1417aa0673b64675ae491edbb6fda3b
unknown <larysamendes@gmail.com>
2024-05-04 20:28:15 -0300

    Implementando o algoritmo de SSTF

---
+++ src/SSTF.py
@@ -0,0 +1,42 @@
+def latencia_sstf(bloco_atual, bloco_destino, houve_seek=False):
+    rotacao = 2  # 2ms de tempo de rotação
+    transferencia = 4 / 125  # Tempo de transferência de 4KB a 125MB/s
+    
+    if houve_seek or bloco_atual // 8 != bloco_destino // 8: #Cada trilha possui 8 segmentos
+        seek = 4 
+    else:
+        seek = 0
+    
+    latencia_total = seek + rotacao + transferencia
+    return latencia_total
+
+def sstf_com_latency(requisicoes, bloco_inicial):
+    bloco_atual = bloco_inicial
+    total_latencia = 0
+    
+    requisicoes.sort()
+
+    while requisicoes:
+        
+        proxima_requisicao = min(requisicoes, key=lambda x: abs(x - bloco_atual))
+        houve_seek = bloco_atual // 8 != proxima_requisicao // 8
+        total_latencia += latencia_sstf(bloco_atual, proxima_requisicao, houve_seek)
+        bloco_atual = proxima_requisicao
+        requisicoes.remove(proxima_requisicao)  # Remove a requisição atendida
+
+    return total_latencia
+
+lista_requisicoes1 = [90, 35, 71, 14, 56, 25, 83, 160, 46, 48]
+bloco_inicial = 5
+total_latencia = sstf_com_latency(lista_requisicoes1, bloco_inicial)
+print("Tempo total de latência:", total_latencia, "ms")
+
+
+lista_requisicoes2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+bloco_inicial = 5
+total_latencia = sstf_com_latency(lista_requisicoes2, bloco_inicial)
+print("Tempo total de latência:", total_latencia, "ms")
+
+lista_requisicoes3 = [2, 3, 92, 567, 421, 689, 134, 876, 349, 598, 5]
+total_latencia = sstf_com_latency(lista_requisicoes3, bloco_inicial)
+print("Tempo total de latência:", total_latencia, "ms")
